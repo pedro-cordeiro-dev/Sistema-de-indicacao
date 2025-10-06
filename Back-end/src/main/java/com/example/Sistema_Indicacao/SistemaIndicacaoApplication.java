57d777b61636dba48145bd7f43bdd0456b135a73
pedro <pedrolucassaraiva_@outlook.com>
2025-10-06 16:04:02 -0300

    adicionando projeto criado no spring Initializr

---
+++ Back-end/src/main/java/com/example/Sistema_Indicacao/SistemaIndicacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sistema_Indicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaIndicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaIndicacaoApplication.class, args);
+	}
+
+}
