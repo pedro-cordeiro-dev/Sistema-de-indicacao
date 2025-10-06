22068d271a345d13869872287fb41500b9052a9b
pedro <pedrolucassaraiva_@outlook.com>
2025-10-06 16:38:45 -0300

    dando os primeiros passos para a autenticação de cadastro e login com spring security

---
+++ Back-end/src/main/java/com/example/Sistema_Indicacao/Repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.example.Sistema_Indicacao.Repository;
+
+import com.example.Sistema_Indicacao.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario,String> {
+
+
+
+}
---
+++ Back-end/src/main/java/com/example/Sistema_Indicacao/Service/TokenService.java
@@ -0,0 +1,38 @@
+package com.example.Sistema_Indicacao.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.example.Sistema_Indicacao.model.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+    public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String GerarToken(Usuario usuario){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String token = JWT.create()
+                    .withIssuer("auth-login")
+                    .withSubject(usuario.getEmail())
+                    .withExpiresAt(this.dataExpiracao())
+                    .sign(algorithm);
+            return token;
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro na autenticação");
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-3"));
+    }
+
+}
