ed1e05a64022163d5060c2a3f86b47e79c5d051e
pedro <pedrolucassaraiva_@outlook.com>
2025-10-07 01:44:03 -0300

    criado e finalizado a logica do link de indicação do projeto

---
+++ Back-end/src/main/java/com/example/Sistema_Indicacao/Service/Security/GeradorLinkIndicacaoService.java
@@ -0,0 +1,29 @@
+package com.example.Sistema_Indicacao.Service.Security;
+
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.HexFormat;
+
+@Service
+public class GeradorLinkIndicacaoService {
+
+    public String GeradorHash(String input) {
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(input.getBytes(StandardCharsets.UTF_8));
+            String codigo = Base64.getUrlEncoder().encodeToString(hash);
+
+            return codigo.substring(0, 10);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+}
